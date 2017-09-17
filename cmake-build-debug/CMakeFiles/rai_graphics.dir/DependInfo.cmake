f6bb973b845a5ba3b0a0d2fbdf419eaeb83001f6
donghokang <east0822@gmail.com>
2017-09-17 22:25:35 +0200

    raiGraphics as independent package

---
+++ cmake-build-debug/CMakeFiles/rai_graphics.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/donghokang/git/raiGraphics/RAI_graphics.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/RAI_graphics.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/imp/camera.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/imp/camera.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/imp/display.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/imp/display.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/imp/obj_loader.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/imp/obj_loader.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/imp/shader.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/imp/shader.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/imp/shader_background.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/imp/shader_background.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/imp/shader_basic.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/imp/shader_basic.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/imp/texture.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/imp/texture.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/imp/vector3d.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/imp/vector3d.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/ANYmal.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/ANYmal.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/Arrow.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/Arrow.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/Background.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/Background.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/Biped.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/Biped.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/Biped_ghost.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/Biped_ghost.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/Biped_simplified.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/Biped_simplified.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/CheckerBoard.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/CheckerBoard.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/CheckerBoard_half.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/CheckerBoard_half.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/Cone.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/Cone.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/Cylinder.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/Cylinder.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/Mesh.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/Mesh.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/Object.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/Object.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/Sphere.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/Sphere.cpp.o"
+  "/home/donghokang/git/raiGraphics/src/obj/SuperObject.cpp" "/home/donghokang/git/raiGraphics/cmake-build-debug/CMakeFiles/rai_graphics.dir/src/obj/SuperObject.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/eigen3"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/cmake_install.cmake
@@ -0,0 +1,71 @@
+# Install script for directory: /home/donghokang/git/raiGraphics
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "Debug")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/donghokang/git/raiGraphics/cmake-build-debug/include/RAI_graphics.hpp;/home/donghokang/git/raiGraphics/cmake-build-debug/include/RAI_keyboard.hpp")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/donghokang/git/raiGraphics/cmake-build-debug/include" TYPE FILE FILES
+    "/home/donghokang/git/raiGraphics/RAI_graphics.hpp"
+    "/home/donghokang/git/raiGraphics/RAI_keyboard.hpp"
+    )
+endif()
+
+if("${CMAKE_INSTALL_COMPONENT}" STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
+  list(APPEND CMAKE_ABSOLUTE_DESTINATION_FILES
+   "/home/donghokang/git/raiGraphics/cmake-build-debug/lib/librai_graphics.a")
+  if(CMAKE_WARN_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(WARNING "ABSOLUTE path INSTALL DESTINATION : ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+  if(CMAKE_ERROR_ON_ABSOLUTE_INSTALL_DESTINATION)
+    message(FATAL_ERROR "ABSOLUTE path INSTALL DESTINATION forbidden (by caller): ${CMAKE_ABSOLUTE_DESTINATION_FILES}")
+  endif()
+file(INSTALL DESTINATION "/home/donghokang/git/raiGraphics/cmake-build-debug/lib" TYPE STATIC_LIBRARY FILES "/home/donghokang/git/raiGraphics/cmake-build-debug/librai_graphics.a")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/home/donghokang/git/raiGraphics/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
