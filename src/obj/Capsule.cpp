dd8378bc03b5ee96f4d3270f3edeecccd8a59d9a
Dongho Kang <east0822@gmail.com>
2017-11-29 18:33:19 +0100

    capsule fixed.

--- src/obj/Capsule.cpp
+++ src/obj/Capsule.cpp
@@ -11,8 +11,9 @@ Capsule::Capsule(float r, float l) {
   com = glm::vec3(0, 0, 0);
 
   int slices = 20;
-  int rings = 10;   // hemisphere
+  int rings = slices/2;   // hemisphere
   int position = 0;
+  int position2 = 0;
 
   float const PHI = 0.5 / (float) (rings - 1);
   float const THETA = 1. / (float) (slices - 1);
@@ -41,28 +42,6 @@ Capsule::Capsule(float r, float l) {
     }
   }
 
-  // side
-  position += rings * rings * 2;
-  for (int i = 0; i < slices; i++) {
-    float theta = 2 * M_PI / slices * i;
-    float y = r * std::cos(theta);
-    float x = -r * std::sin(theta);
-    positions.push_back(glm::vec3(x, y, -0.5 * l));
-    normals.push_back(glm::vec3(x, y, 0));
-    positions.push_back(glm::vec3(x, y, 0.5 * l));
-    normals.push_back(glm::vec3(x, y, 0));
-  }
-
-  for (int i = 0; i < slices; i++) {
-    indices.push_back(position + 2 * i);
-    indices.push_back(position + (2 * (i + 1))%(2*slices));
-    indices.push_back(position + 2 * i + 1);
-
-    indices.push_back(position + 2 * i + 1);
-    indices.push_back(position + (2 * (i + 1))%(2*slices));
-    indices.push_back(position + (2 * (i + 1) + 1)%(2*slices));
-  }
-
   // top
   for (phi = 0; phi < rings; phi++) {
     for (theta = 0; theta < rings * 2; theta++) {
@@ -75,7 +54,7 @@ Capsule::Capsule(float r, float l) {
     }
   }
 
-  position += slices * 2;
+  position += rings * rings * 2;
   for (phi = 0; phi < rings - 1; phi++) {
     for (theta = 0; theta < rings * 2 - 1; theta++) {
       indices.push_back(position + phi * rings * 2 + theta);
@@ -86,6 +65,21 @@ Capsule::Capsule(float r, float l) {
       indices.push_back(position + (phi + 1) * rings * 2 + (theta + 1));
     }
   }
+
+  // side
+  position = (rings * 2) * (rings - 1);
+  position2 = rings * rings * 2 + (rings * 2) * (rings - 1);
+  for (int i = 0; i < slices-1; i++) {
+    indices.push_back(position + i);
+    indices.push_back(position + i + 1);
+    indices.push_back(position2 + i + 1);
+
+    indices.push_back(position + i);
+    indices.push_back(position2 + i + 1);
+    indices.push_back(position2 + i);
+  }
+
+
 }
 
 } // object
