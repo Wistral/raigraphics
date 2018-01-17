91dc7890ce17d110fc9f6a8b9ab7445395a528c5
Jemin Hwangbo <jemin.hwangbo@gmail.com>
2018-01-17 23:59:50 +0100

    fix on install script

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 echo 'export RAI_GRAPHICS_OPENGL_ROOT='$PWD'' >> ~/.bashrc
 sudo apt-get install -qq libglew-dev freeglut3-dev libsdl2-dev libglm-dev glee-dev libsdl2-image-dev libassimp-dev libsoil-dev libfreeimage3 libfreeimage-dev libsdl2-ttf-dev
 
+### Check Ubuntu version
+version=$(lsb_release -r | awk '{ print $2 }')
+yrelease=$( echo "$version" | cut -d. -f1 )
+mrelease=$( echo "$version" | cut -d. -f2 )
+
 # ffmpeg
 if [ "$yrelease" -eq "16" ]; then
     sudo apt-get install $APT_GET_FLAGS ffmpeg
