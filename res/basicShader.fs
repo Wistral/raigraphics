a932f13ce75e834f388898af8357a005bcf3b4a4
Jemin Hwangbo <jemin.hwangbo@gmail.com>
2018-02-04 11:04:29 +0100

    quick fix on the previous commit

--- res/basicShader.fs
+++ res/basicShader.fs
@@ -35,8 +35,8 @@ void main()
 
         float scont=pow(max(0.0,dot(surf2view,reflection)),shininess);
         vec3 specular=scont*lspecular*mspecular;
-        color = colorMono;
-        gl_FragColor=vec4(color * (ambient+diffuse+specular)*att,transparency);  //<- don't forget the paranthesis (ambient+diffuse+specular)
+        colorMono = colorMono;
+        gl_FragColor=vec4(colorMono * (ambient+diffuse+specular)*att,transparency);  //<- don't forget the paranthesis (ambient+diffuse+specular)
 
 
 }
