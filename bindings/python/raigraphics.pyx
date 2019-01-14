c71ff099814e851593505a073761c2cc8c003104
east0822@gmail.com <donghokang>
2019-01-14 19:21:41 +0100

    cython wrapper for raigraphics

--- bindings/python/raigraphics.pyx
+++ bindings/python/raigraphics.pyx
@@ -1,15 +1,30 @@
 from libcpp cimport bool
 
+cdef extern from "raiGraphics/obj/Sphere.hpp" namespace "rai_graphics::object":
+    cdef cppclass Sphere:
+        Sphere(float);
+
 cdef extern from "raiGraphics/RAI_graphics.hpp" namespace "rai_graphics":
     cdef cppclass RAI_graphics:
         RAI_graphics(int, int);
         void start();
         void end();
+
+        void addObject(Sphere *obj);
+
         int isQuitting();
 
+cdef class PySphere:
+    cdef Sphere *thisptr
+
+    def __cinit__(self, float radius):
+        self.thisptr = new Sphere(radius)
+
+    def __dealloc__(self):
+        del self.thisptr
 
 cdef class PyRaiGraphics:
-    cdef RAI_graphics *thisptr      # hold a C++ instance which we're wrapping
+    cdef RAI_graphics *thisptr
 
     def __cinit__(self, int window_width, int window_height):
         self.thisptr = new RAI_graphics(window_width, window_height)
@@ -25,3 +40,6 @@ cdef class PyRaiGraphics:
 
     def isQuitting(self):
         return self.thisptr.isQuitting()
+
+    def addSphere(self, PySphere sphere):
+        return self.thisptr.addObject(sphere.thisptr)
--- bindings/python/test.py
+++ bindings/python/test.py
@@ -1,7 +1,11 @@
-from raigraphics import PyRaiGraphics
+from raigraphics import PyRaiGraphics, PySphere
 from time import sleep
 
 sim = PyRaiGraphics(800, 600)
+
+sphere = PySphere(1.0)
+sim.addSphere(sphere)
+
 sim.start()
 
 while not sim.isQuitting():
