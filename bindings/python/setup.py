dcea881d94f3c69d151a478ff80a405f4b3b88c8
east0822@gmail.com <donghokang>
2019-01-16 18:16:08 +0100

    pyraigraphics_path import for path variables

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -4,46 +4,40 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
-PROJECT_NAME = 'pyraigraphics'
-
-# source
-ROOT_DIR = '../../'                 # project root
-SRC_DIR = ROOT_DIR                  # source dir
-HDR_DIR = ROOT_DIR + 'include'      # header dir
+import pyraigraphics_path
 
 # compiler
-COMPILER = '/usr/bin/g++-7'         # compiler path
-os.environ["CC"] = COMPILER
-os.environ["CXX"] = COMPILER
-
-# libs
-EIGEN = '/usr/include/eigen3'       # eigen path
-BOOST = '/usr/lib/x86_64-linux-gnu' # boost path
+os.environ["CC"] = pyraigraphics_path.COMPILER
+os.environ["CXX"] = pyraigraphics_path.COMPILER
 
 ############
 
 raigraphics_extension = Extension(
-    PROJECT_NAME,
-    sources=['raigraphics.pyx'],
-    libraries=['raigraphics',
-               'GL',
-               'GLEW',
-               'SDL2',
-               'SDL2_ttf',
-               'assimp',
-               'SOIL',
-               'pthread',
-               'boost_thread',
-               'boost_system',
-               'boost_filesystem'],
-    library_dirs=['/usr/local/lib', '/usr/lib', BOOST],
-    include_dirs=[HDR_DIR],
-    extra_compile_args=['-std=c++11', '-pthread', '-g', '-isystem' + EIGEN],
+    pyraigraphics_path.PROJECT_NAME,
+    sources=[
+        'raigraphics.pyx',
+    ],
+    libraries=[
+        'raigraphics',
+        'GL',
+        'GLEW',
+        'SDL2',
+        'SDL2_ttf',
+        'assimp',
+        'SOIL',
+        'pthread',
+        'boost_thread',
+        'boost_system',
+        'boost_filesystem',
+    ],
+    library_dirs=['/usr/local/lib', '/usr/lib', pyraigraphics_path.BOOST],
+    include_dirs=[pyraigraphics_path.HDR_DIR],
+    extra_compile_args=['-std=c++11', '-pthread', '-g', '-isystem' + pyraigraphics_path.EIGEN],
     language='c++',
 )
 
 setup(
-    name=PROJECT_NAME,
+    name=pyraigraphics_path.PROJECT_NAME,
     author='donghokang',
     ext_modules=cythonize([raigraphics_extension]),
 )
